c9e9eccde9621700af1c0cfc8e4a8a1fd4a95f44
KetyJomes <jomessofiaketlyn@gmail.com>
2026-02-09 09:18:24 -0300

    Aula03 - Port e caminhos

---
+++ Aula03/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const port = 8080
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({response: "Api funcionando!"})
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
