142d5b269c628e0baf6ffc01d4794dc6ddbae83f
KetyJomes <jomessofiaketlyn@gmail.com>
2026-02-09 11:37:28 -0300

    Aula03 - Registrando Usuários

--- Aula03/src/routes/person.ts
+++ Aula03/src/routes/person.ts
@@ -6,7 +6,11 @@ const people: object[] = [];
 router
     .post('/registrar',(req: Request,res: Response) =>{
         const{name, lastname} = req.body
-        console.log(name,lastname)
+        people.push({name,lastname})
+        res.status(200).send({massage: `Usuário ${name} ${lastname} cadastrado com sucesso!`})
+    })
+    .get('/usuarios',(req: Request,res: Response) =>{
+        res.status(200).send({users: people})
     })
 //....
 
