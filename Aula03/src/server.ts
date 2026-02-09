4d4ae8fc1b30fb03984f7b7518c390d51fc43708
KetyJomes <jomessofiaketlyn@gmail.com>
2026-02-09 09:31:29 -0300

    Aula03 - Caminhos e array

--- Aula03/src/server.ts
+++ Aula03/src/server.ts
@@ -3,14 +3,26 @@ import express from 'express'
 const port = 8080
 const app = express()
 
+
 const pessoa = {
     name: "Ketlyn",
     lastname: "Jomes"
 }
 
+// array:
+const pessoas = [
+    {
+        name: "Sofia"
+    },
+    {
+        name: "Ketlyn"
+    }
+
+]
+
 // correto:
 app.get('/objeto', (req, res) => {
-    res.send({pessoa: pessoa})
+    res.send({pessoas: pessoas})
 })
 // outro exmplo:
 app.get('/direto', (req, res) => {
