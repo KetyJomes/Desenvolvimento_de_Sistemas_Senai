7d4a40065041bf1db6dd07d8f0eb08ff026b527e
KetyJomes <jomessofiaketlyn@gmail.com>
2026-02-09 09:27:23 -0300

    Aula03 - Colocando nome nos caminhos

--- Aula03/src/server.ts
+++ Aula03/src/server.ts
@@ -3,8 +3,18 @@ import express from 'express'
 const port = 8080
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({response: "Api funcionando!"})
+const pessoa = {
+    name: "Ketlyn",
+    lastname: "Jomes"
+}
+
+// correto:
+app.get('/objeto', (req, res) => {
+    res.send({pessoa: pessoa})
+})
+// outro exmplo:
+app.get('/direto', (req, res) => {
+    res.send({pessoa})
 })
 
 app.listen(port, () => {
