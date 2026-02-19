59ee8fb1e53cab1aa1d11c57e59b74869d0dd7a8
KetyJomes <jomessofiaketlyn@gmail.com>
2026-02-19 10:54:59 -0300

    Continuação aula 03 - PUT

--- Aula03/src/routes/person.ts
+++ Aula03/src/routes/person.ts
@@ -19,7 +19,12 @@ router
     })
      .get('/filtro',(req: Request,res: Response) =>{
         const {name, lastname} = req.query
-        res.status(200).send({response: name, lastname})
+        res.status(200).send({response: `${name}, ${lastname}`})
+    })
+     .put('/atualizar/:id',(req: Request,res: Response) =>{
+        const {id} = req.params
+        const {name, lastname} = req.body
+        res.status(200).send({response: `Atualizando o uruário ${id} -> ${name} ${lastname}`})
     })
 
 export default router;
