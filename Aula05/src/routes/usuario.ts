48f6f524ddbb195f33b844014d8ef3309400e052
KetyJomes <jomessofiaketlyn@gmail.com>
2026-03-02 10:31:54 -0300

    Continuação Aula05 - Controllers

--- Aula05/src/routes/usuario.ts
+++ Aula05/src/routes/usuario.ts
@@ -1,39 +1,12 @@
-import express, { Request, response, Response, Router } from 'express';
-import Person from '../models/Person.ts'
-
+import express, { Router } from 'express';
+import PersonController from '../controllers/PersonController.ts';
 
 const router: Router = express.Router();
 
-
 router
-    .post('/registrar', async(req: Request,res: Response) =>{
-        const{nome, sobrenome, idade} = req.body
-        const person = new Person({nome, sobrenome, idade})
-        await person.save()
-        res.status(200).send("Registrado com sucesso!")
-    })
-    .get('/consultar', async (req: Request, res: Response) => {
-        const people = await Person.find()
-        res.status(200).send({response:people})
-    })
-    .get('/consultarUma', async (req: Request, res: Response) => {
-        const people = await Person.find({nome:"Ketlyn"})
-        res.status(200).send({response:people})
-    })
-    .get('/filtro',(req: Request,res: Response) =>{
-      
-    })
-    .get('/consultar/:id', (req: Request, res: Response) => {
-  
-    })
-    .put('/atualizar/:id',(req: Request,res: Response) =>{
-        
-    })
-    .patch('/atualizando/:id',(req: Request,res: Response) => {
-
-    })
-    .delete('/deletando/:id',(req: Request,res: Response) => {
-        
-    })
+    .get('/usuarios', PersonController.getUsers)
+    .post('/cadastrar', PersonController.postUsers)
+    .put('/alterar', PersonController.putUsers)
+    .delete('/deletar', PersonController.deleteUsers)
 
 export default router;
