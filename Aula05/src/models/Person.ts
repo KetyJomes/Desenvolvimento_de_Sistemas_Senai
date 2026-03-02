fc5af6a039e9809f5538bc0eb06c509005446187
KetyJomes <jomessofiaketlyn@gmail.com>
2026-03-02 11:43:04 -0300

    Continuação Aula05 - Middlewares (validação)

--- Aula05/src/models/Person.ts
+++ Aula05/src/models/Person.ts
@@ -1,13 +1,13 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface IPerson extends Document {
-nome: string;
-email: string;
+    nome: string;
+    email: string;
 }
 
 const personSchema: Schema = new Schema({
-nome: { type: String, required: true },
-email: { type: String, required: true },
+    nome: { type: String, required: true },
+    email: { type: String, required: true },
 
 });
 
--- Aula05/src/routes/usuario.ts
+++ Aula05/src/routes/usuario.ts
@@ -1,11 +1,11 @@
 import express, { Router } from 'express';
 import PersonController from '../controllers/PersonController.ts';
-
+import { validateRegistor } from '../middlewares/personMiddlewares.ts';
 const router: Router = express.Router();
 
 router
     .get('/usuarios', PersonController.getUsers)
-    .post('/cadastrar', PersonController.postUsers)
+    .post('/cadastrar', validateRegistor, PersonController.postUsers)
     .put('/alterar/:id', PersonController.putUsers)
     .delete('/deletar/:id', PersonController.deleteUsers)
 
