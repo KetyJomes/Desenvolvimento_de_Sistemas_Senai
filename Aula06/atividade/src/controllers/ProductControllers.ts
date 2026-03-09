76e758ec1edaf8a815c441aa8e06d655c4198bcb
KetyJomes <jomessofiaketlyn@gmail.com>
2026-03-09 09:45:25 -0300

    Aula06 - Continuação da atividade

--- Aula06/atividade/src/controllers/ProductControllers.ts
+++ Aula06/atividade/src/controllers/ProductControllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import Product from '../models/productModel'
+import Product from '../models/ProductModel'
 
 class ProductController {
     static async postProduct(req: Request, res: Response){
@@ -7,7 +7,7 @@ class ProductController {
         const product = new Product({name, description, price, stock, category})
         await product.save()
        
-        return res.status(200).send("Produto cadastrado com sucesso!")
+        return res.status(200).send({message: "Produto cadastrado com sucesso!"})
     }
     static async getProduct(req: Request, res: Response){
         const product = await Product.find()
@@ -18,26 +18,19 @@ class ProductController {
 
     const product = await Product.findById(id);
 
-    if (!product) {
-        return res.status(404).send({ message: "Produto não encontrado" });
-    }
-
     return res.status(200).send(product);
 }
     static async updateProduct(req: Request, res: Response){
         const {id} = req.params
         const {name, description, price, stock, category} = req.body
         await Product.findByIdAndUpdate(id, { name, description, price, stock, category});
-        return res.status(200).send("Produto atualizado!")
+        return res.status(200).send({message: "Produto atualizado!"})
     }
     static async deleteProduct(req: Request, res: Response){
         const {id} = req.params
         const existProduct = await Product.findByIdAndDelete(id);
-        
-        if (!existProduct) {
-        return res.status(404).send({ message: "Produto não encontrado" });
-        }
-        return res.status(200).send("Produto deletado!")
+        return res.status(200).send({message: "Produto deletado!"})
+            
     }
 }
 
--- Aula06/atividade/src/routes/product.ts
+++ Aula06/atividade/src/routes/product.ts
@@ -1,12 +1,13 @@
 import express, { Router } from 'express';
-import { validateUpdate } from '../middlewares/productMiddlewares.ts';
-import ProductController from '../controllers/productControllers.ts';
+import { validateUpdate, validateGet, validateDelete } from '../middlewares/ProductMiddlewares.ts';
+import ProductController from '../controllers/ProductControllers.ts';
 const router: Router = express.Router();
 
 router
     .get('/product',ProductController.getProduct)
+    .get('/productFilter/:id', validateGet, ProductController.getProductFilter)
     .post('/register', ProductController.postProduct)
-    .put('/update/:id', ProductController.updateProduct)
-    .delete('/delete/:id', ProductController.deleteProduct)
+    .put('/update/:id', validateUpdate, ProductController.updateProduct)
+    .delete('/delete/:id', validateDelete, ProductController.deleteProduct)
 
 export default router;
